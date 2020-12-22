9c0c8a2d37a63b9e05489c5d5bc35b036cab6b91
Michael Farber Brodsky <taco@mailo.com>
2020-12-22 12:20:23 +0200

    Add more operations

--- src/App.jsx
+++ src/App.jsx
@@ -22,16 +22,20 @@ function App() {
           />
         </div>
         <div className={styles.twoMatricesOperators}>
+          <button onClick={() => { let temp = M1; setM1(M2); setM2(temp); }}>Swap A, B</button>
           { M1.cols === M2.rows ? (
             <button onClick={() => setResult(M1.mul(M2))}>A ⨉ B</button>
           ) : null}
           { M1.rows === M2.rows && M1.cols === M2.cols ? (
             <button onClick={() => setResult(M1.add(M2))}>A + B</button>
           ) : null}
+          <button onClick={() => setResult(M1.transpose())}>A transpose</button>
           { M1.rows === M1.cols ? (
             <div className={styles.twoMatricesOperators}>
               <p><b>Square Matrix Operations</b></p>
-              <button onClick={() => setM2(M1.inverse())}>B ➞ Inverse A</button>
+              <button onClick={() => setResult(M1.inverse())}>Inverse A</button>
+              <button onClick={() => setResult(new Matrix([M1.det()], 1, 1))}>det(A)</button>
+              <button onClick={() => setResult(M1.adj())}>adj(A)</button>
             </div>
             ) : null}
         </div>
@@ -47,9 +51,14 @@ function App() {
       </div>
       <h1>Result:</h1>
       { result ? (
-        <MatrixComponent
-          matrix={result}
-        />
+        <div>
+          <MatrixComponent
+            matrix={result}
+          />
+          <br /><br />
+          <button onClick={() => setM1(result)}>Save as A</button>
+          <button onClick={() => setM2(result)}>Save as B</button>
+        </div>
       ) : null}
     </div>
   );
